e75539663f83df8cbb10d6553ba510c13fa3e228
Mihir Chaple <mihirchaple005@gmail.com>
2024-08-11 19:59:40 +0530

    cleaning app.jsx

--- Frontend/onestepsocialfrontend/src/App.jsx
+++ Frontend/onestepsocialfrontend/src/App.jsx
@@ -1,25 +1,16 @@
 import './App.css'
-<<<<<<< HEAD
 import Onestepsocial from './components/Onestepsocial.component'
 
 
-=======
-//import Layout from './components/Layout/Layout'
-import Onestepsocial from './components/Onestepsocial'
->>>>>>> 294f1210dcdb9fdc2b4592deb7d5e5689b123d48
 
 function App() {
 
   
   return (
     <>
-<<<<<<< HEAD
-    
+
       <Onestepsocial />
-  
-=======
-      <Onestepsocial/>
->>>>>>> 294f1210dcdb9fdc2b4592deb7d5e5689b123d48
+
     </>
   )
 }
