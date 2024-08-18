4d572dc9175b26bc33be9560cb88f9ee73c459e0
mananmaheshwari11 <maheshwarimanan2211@gmail.com>
2024-08-18 10:58:56 +0530

    Updated Footer

--- Frontend/onestepsocialfrontend/src/App.jsx
+++ Frontend/onestepsocialfrontend/src/App.jsx
@@ -1,17 +1,14 @@
-import './App.css'
+// import './App.css'
+import Layout from './components/Layout/Layout'
 import Onestepsocial from './components/Onestepsocial.component'
-
-
 
 function App() {
 
   
   return (
-    <>
-
+    <Layout>
       <Onestepsocial />
-
-    </>
+    </Layout>
   )
 }
 
